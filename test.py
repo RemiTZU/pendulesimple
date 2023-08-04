e43be64a14a9d5c36e230b78b8a64e6e15f81581
RemiTZU <bonnetremi74@gmail.com>
2023-08-04 12:01:14 +0200

    newcommit

--- test.py
+++
@@ -1,9 +0,0 @@
-import csv
-import os
-
-print("Répertoire de travail:", os.getcwd())
-
-
-with open('data.csv', 'w', newline='') as fichier:
-    writer = csv.writer(fichier)
-    writer.writerow([1, 2])
